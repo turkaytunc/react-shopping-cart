d72dec3d982153cd4b84f7c5bca2a3c109cc9611
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-25 20:31:04 +0300

    resim eklendi

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="200x200 image" /> */}
+        <img src={this.state.imageUrl} alt="200x200 image" />
         <span className="badge badge-primary m-2">{this.formatCount()}</span>
         <button style={[this.styles][0]} className="btn btn-secondary btn-sm">
           Increment
