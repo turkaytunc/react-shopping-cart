2a7be1cf3feef1cfbe6491149e27150dd67e60e3
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-25 20:21:09 +0300

    Updates

---
+++ precache-manifest.a4c650b9bc89d4db458e5c5227035317.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f1855b86347b4740b018ab7480bb66",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "fee9507ac1f1e24a76d5",
+    "url": "/react-shopping-cart/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f8313a6fd2f4a1c58d85",
+    "url": "/react-shopping-cart/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fee9507ac1f1e24a76d5",
+    "url": "/react-shopping-cart/static/js/2.7b315c85.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-shopping-cart/static/js/2.7b315c85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8313a6fd2f4a1c58d85",
+    "url": "/react-shopping-cart/static/js/main.cf6c6131.chunk.js"
+  },
+  {
+    "revision": "52c442591349bcf526f5",
+    "url": "/react-shopping-cart/static/js/runtime-main.e775b367.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-shopping-cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
